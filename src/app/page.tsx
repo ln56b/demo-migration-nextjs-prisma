c9c6bb3bd07de7045f08c58c9c72959bd99b8c82
Hélène BAUDRAND <Helene@MacBook-Pro-de-apixis.local>
2024-04-05 16:39:43 +0200

    refactor(css only): v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,12 @@ export default async function Home() {
   const todos = await getPrismaTodos();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CreateTodo />
-      <Todos todos={todos} />
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl font-bold pb-10">TODO App V1</h1>
+      <div className="flex flex-col justify-between">
+        <CreateTodo />
+        <Todos todos={todos} />
+      </div>
     </main>
   );
 }
